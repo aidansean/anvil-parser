cca539d320ddb2763784a0943f0d5a3b45101d66
Aidan Randle-Conde <aidan.randleconde@gmail.com>
2024-01-01 14:21:49 +0000

    Applied Black. Updated docstrings and variable names.

--- anvil/block.py
+++ anvil/block.py
@@ -2,6 +2,7 @@ from nbt import nbt
 from frozendict import frozendict
 from .legacy import LEGACY_ID_MAP
 
+
 class Block:
     """
     Represents a minecraft block.
@@ -15,21 +16,23 @@ class Block:
     properties: :class:`dict`
         Block properties as a dict
     """
-    __slots__ = ('namespace', 'id', 'properties')
 
-    def __init__(self, namespace: str, block_id: str=None, properties: dict=None):
+    __slots__ = ("namespace", "id", "properties")
+
+    def __init__(self, namespace: str, block_id: str = None, properties: dict = None):
         """
         Parameters
         ----------
         namespace
-            Namespace of the block. If no block_id is given, assume this is ``block_id`` and set namespace to ``"minecraft"``
+            Namespace of the block.
+            If no block_id is given, assume this is ``block_id`` and set namespace to ``"minecraft"``
         block_id
             ID of the block
         properties
             Block properties
         """
         if block_id is None:
-            self.namespace = 'minecraft'
+            self.namespace = "minecraft"
             self.id = namespace
         else:
             self.namespace = namespace
@@ -40,15 +43,19 @@ class Block:
         """
         Returns the block in the ``minecraft:block_id`` format
         """
-        return self.namespace + ':' + self.id
+        return self.namespace + ":" + self.id
 
     def __repr__(self):
-        return f'Block({self.name()})'
+        return f"Block({self.name()})"
 
     def __eq__(self, other):
         if not isinstance(other, Block):
             return False
-        return self.namespace == other.namespace and self.id == other.id and self.properties == other.properties
+        return (
+            self.namespace == other.namespace
+            and self.id == other.id
+            and self.properties == other.properties
+        )
 
     def __hash__(self):
         return hash(self.name()) ^ hash(frozendict(self.properties))
@@ -65,7 +72,7 @@ class Block:
         , args, kwargs
             Will be passed on to the main constructor
         """
-        namespace, block_id = name.split(':')
+        namespace, block_id = name.split(":")
         return cls(namespace, block_id, *args, **kwargs)
 
     @classmethod
@@ -78,14 +85,14 @@ class Block:
         tag
             Raw tag from a section's palette
         """
-        name = tag['Name'].value
-        properties = tag.get('Properties')
+        name = tag["Name"].value
+        properties = tag.get("Properties")
         if properties:
             properties = dict(properties)
         return cls.from_name(name, properties=properties)
 
     @classmethod
-    def from_numeric_id(cls, block_id: int, data: int=0):
+    def from_numeric_id(cls, block_id: int, data: int = 0):
         """
         Creates a new Block from the block_id:data fromat used pre-flattening (pre-1.13)
 
@@ -98,11 +105,12 @@ class Block:
         """
         # See https://minecraft.gamepedia.com/Java_Edition_data_value/Pre-flattening
         # and https://minecraft.gamepedia.com/Java_Edition_data_value for current values
-        key = f'{block_id}:{data}'
+        key = f"{block_id}:{data}"
         if key not in LEGACY_ID_MAP:
-            raise KeyError(f'Block {key} not found')
+            raise KeyError(f"Block {key} not found")
         name, properties = LEGACY_ID_MAP[key]
-        return cls('minecraft', name, properties=properties)
+        return cls("minecraft", name, properties=properties)
+
 
 class OldBlock:
     """
@@ -115,9 +123,10 @@ class OldBlock:
     data: :class:`int`
         The block data, used to represent variants of the block
     """
-    __slots__ = ('id', 'data')
 
-    def __init__(self, block_id: int, data: int=0):
+    __slots__ = ("id", "data")
+
+    def __init__(self, block_id: int, data: int = 0):
         """
         Parameters
         ----------
@@ -133,7 +142,7 @@ class OldBlock:
         return Block.from_numeric_id(self.id, self.data)
 
     def __repr__(self):
-        return f'OldBlock(id={self.id}, data={self.data})'
+        return f"OldBlock(id={self.id}, data={self.data})"
 
     def __eq__(self, other):
         if isinstance(other, int):
